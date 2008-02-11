c5e3572bec128e38e48ed8b9f9b80ab800d3af85
Harri Pitkänen <hatapitk@iki.fi>
2008-02-11 18:55:22 +0000

    * Remove gtkmm sources, require external version of that   library instead.
    git-svn-id: https://voikko.svn.sourceforge.net/svnroot/voikko/trunk/tmispell@1984 ee56993d-3414-0410-8ed4-8136e77a0dd8

--- src/glibmm/buildconvert.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-perl -pi -e 's,<(glibmm/.*)>,"$1",g' *.cc *.h private/*.h
-perl -pi -e 's,<(glibmmconfig.h)>,"glibmm/$1",g' *.cc *.h private/*.h
-perl -pi -e 's,^.*wrap_init.*$,,' *.cc
-perl -pi -e 's,^#undef.*$,,' glibmmconfig.h
